32691ff761c4a80bc8160017bcf60baefcf06a40
Seitek <stefan.schnieder.it@gmail.com>
2021-01-25 18:28:59 +0100

    Theme Provider

---
+++ src/themes/theme.js
@@ -0,0 +1,26 @@
+export default{
+    fonts: {
+        main: "Muli, sans-serif",
+        code: "Roboto Mono, monospace",
+    },
+    colors: {
+        main1: "hsl(207,70%,59%)",
+        main2: "hsl(207,70%,94%)",
+    },
+    breakpoints: {
+        mobile: "only screen and (max-width: 50rem)",
+        tablet: "only screen and (max-width: 65rem)",
+    },
+    spacings: {
+        small: "1rem",
+        medium: "2rem",
+        large: "3rem",
+    },
+    animations: {
+        button: "box-shadow 0.3s ease",
+        link:"color 0.2s ease",
+    },
+    shadows: {
+        shadow1: "0px 5px 20px rgba(30,30,31,0.05)",
+    },
+}
